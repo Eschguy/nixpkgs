78f3321c41780a7026dd7a13d0fa33d3a91f8b28
Austin Eschweiler <austin.eschweiler@gmail.com>
2025-10-01 15:22:51 -0500

    add desktop item and icon

--- pkgs/by-name/tt/tts-mod-vault/package.nix
+++ pkgs/by-name/tt/tts-mod-vault/package.nix
@@ -3,6 +3,7 @@
   flutter329,
   makeDesktopItem,
   fetchFromGitHub,
+  fetchurl,
 }:
 
 flutter329.buildFlutterApplication rec {
@@ -16,16 +17,26 @@ flutter329.buildFlutterApplication rec {
     hash = "sha256-BTs+4QeyVJeg415uiNXww8twQwUInHfB8voWJjeVs20=";
   };
 
+  icon = fetchurl {
+    url = "https://github.com/markomijic/TTS-Mod-Vault/raw/9a8eec2b009ee4b653863fa3d553a309a2d07431/assets/icon/tts_mod_vault_icon.png";
+    hash = "sha256-xFQpVciuBdOxHCdK4IrskPd0gPeb3x5wklYLlR9vvnY=";
+  };
+
   pubspecLock = lib.importJSON ./pubspec.lock.json;
 
   desktopItem = makeDesktopItem {
     name = "tts-mod-vault";
     exec = "tts-mod-vault";
-    icon = "tts_mod_vault_icon";
+    icon = icon;
     comment = "Tabletop Simulator Mod Vault";
     desktopName = "TTS Mod Vault";
     categories = [ "Utility" ];
   };
+
+  postInstall = ''
+    install -m 444 -D ${desktopItem}/share/applications/tts-mod-vault.desktop $out/share/applications/tts-mod-vault.desktop
+    install -m 444 -D ${icon} $out/share/icons/hicolor/512x512/apps/tts-mod-vault.png
+  '';
 
   meta = {
     description = "Download and backup assets for your Tabletop Simulator mods";
